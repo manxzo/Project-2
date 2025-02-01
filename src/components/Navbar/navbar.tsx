8b1243229fd400bed33cbfe67d84cb8be0cdc96a
manxzo <manzoball@gmail.com>
2025-02-02 00:48:46 +0800

    -Edited existing Navbar in order to accomodate for my app -Removed unnecessary components -created a new LocationPopup in order to allow User to change their Location -It also auto detects the users location using IPInfo API and checks it against a list of locations supported by Adzuna API and defaults to sg if not supported. -Added Location Popup to navbar -Enabled the preconfigured ThemeSwitcher in navbar

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,50 @@
+import { Link } from "@heroui/link";
+import {
+  Navbar as HeroUINavbar,
+  NavbarContent,
+  NavbarItem,
+} from "@heroui/navbar";
+import { link as linkStyles } from "@heroui/theme";
+import clsx from "clsx";
+import LocationPopup from "./Components/LocationPopup";
+import { siteConfig } from "@/config/site";
+import { ThemeSwitch } from "@/components/theme-switch";
+import { GithubIcon } from "@/components/icons";
+
+export const Navbar = () => {
+  return (
+    <HeroUINavbar maxWidth="xl" position="sticky" isBordered>
+      <NavbarContent className="basis-1/5 sm:basis-full " justify="center">
+        <div className="hidden 2xl:flex gap-20 justify-evenly  ml-2">
+          {siteConfig.navItems.map((item) => (
+              <NavbarItem key={item.href}>
+                <Link
+                  className={clsx(
+                    linkStyles({
+                      color: "primary",
+                      size: "lg",
+                      underline: "hover",
+                    }),
+                    "data-[active=true]:text-primary data-[active=true]:after:bg-primary data-[active=true]:font-large"
+                  )}
+                  color="foreground"
+                  href={item.href}
+                >
+                  {item.label}
+                </Link>
+              </NavbarItem>
+            
+          ))}
+        </div>
+
+        <NavbarItem>
+          <LocationPopup />
+        </NavbarItem>
+        <Link isExternal href={siteConfig.links.github}>
+          <GithubIcon className="text-default-500" />
+        </Link>
+        <ThemeSwitch />
+      </NavbarContent>
+    </HeroUINavbar>
+  );
+};
