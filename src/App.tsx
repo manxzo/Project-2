4fd28cad49bf0b4968d6ad9de190f53298f834ef
manxzo <manzoball@gmail.com>
2025-02-02 02:40:50 +0800

    ft:updated Routes in App for Resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Search from "./components/Search/Search";
 import DefaultLayout from "./layouts/default";
+import Resume from "./components/Resume/Resume";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home"/>}/>
       <Route path="/home" element={<DefaultLayout><h1>Home</h1></DefaultLayout>} />
       <Route path="/search" element={<Search />} />
+      <Route path="/resume" element={<Resume/>}/>
     </Routes>
   );
 }
