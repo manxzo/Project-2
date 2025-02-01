05b01fbcab674e642d3a9e5d56b0394d4068f43e
manxzo <manzoball@gmail.com>
2025-02-02 02:40:19 +0800

    ft:updated resume PDF component to test

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,6 +1,12 @@
+import DefaultLayout from "@/layouts/default";
 import ResumeResult from "./Components/ResumeResult";
 import ResumeUpload from "./Components/ResumeUpload";
+import PdfResumeUploader from "./Components/ResumeUploadPDF";
 const Resume = () => {
-
+return(
+    <DefaultLayout>
+     <PdfResumeUploader/>   
+    </DefaultLayout>
+)
 }
 export default Resume;
