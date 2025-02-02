4cb4c5859a5e97f652718fbcb74877100ac47f6a
manxzo <manzoball@gmail.com>
2025-02-02 16:55:09 +0800

    added new resume result working area

--- src/components/Resume/Components/ResumeResult.tsx
+++ src/components/Resume/Components/ResumeResult.tsx
@@ -1,4 +1,14 @@
-const ResumeResult = (props) =>{
+import { Snippet } from "@heroui/react";
 
-}
+const ResumeResult = ({ response, error }) => {
+  const errorMsg = JSON.stringify(error, null, 2);
+  const result = response? response.toString():"No Response";
+
+  return (
+    <Snippet color={result ? "default" : "danger"} className="w-full h-auto p-4 border rounded-md ">
+      <pre className="whitespace-pre-wrap break-words">{result ? result : errorMsg}</pre>
+    </Snippet>
+  );
+};
+
 export default ResumeResult;
