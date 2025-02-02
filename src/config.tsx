187a24138cce340a0b0c0b4b10cae3c26332e79b
manxzo <manzoball@gmail.com>
2025-02-02 22:37:49 +0800

    added new references for easier update to Airtable and to save recent additions to reduce calling data from Airtable and a global message for any errors or issues

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,21 @@
 import React, { createContext, useState, ReactNode } from "react";
 
+interface Job {
+ title: string;
+  company: string;
+  description:string;
+  id:string;
+  location: string;
+  min_salary:string;
+  max_salary:string;
+  date_posted:string;
+}
+interface Resume {
+  id:string;
+  text:string;
+  job_id:string;
+}
+
 interface Config {
   apiKeys: {
     ipInfoKey: string;
@@ -10,11 +26,21 @@ interface Config {
     airtableBase:string;
   };
   country: string;
+  saved:{
+    resumes:Resume[];
+    jobs:Job[];
+  }
+  message:string;
 }
 
 interface ConfigContextType {
   config: Config;
   setConfig: React.Dispatch<React.SetStateAction<Config>>;
+  saveJob: (job:Job)=>void;
+  unsaveJob:(job:Job) => void;
+  saveResume:(resume:Resume) => void;
+  unsaveResume:(resume:Resume)=>void;
+  newMessage:(message:string)=>void;
 }
 
 export const ConfigContext = createContext<ConfigContextType | undefined>(
@@ -32,10 +58,70 @@ export const ConfigProvider = ({ children }: { children: ReactNode }) => {
       airtableBase:import.meta.env.VITE_APP_AIRTABLE_BASE
     },
     country: "sg",
+    saved:{
+      resumes:[],
+      jobs:[],
+    },
+    message:""
   });
-
+  const saveJob = (job: Job) => {
+    setConfig((prev) => {
+      const isSaved = prev.saved.jobs.some(
+        (favJob) => favJob.id === job.id
+      );
+      if (!isSaved) {
+        return {
+          ...prev,
+          saved: {
+            ...prev.saved,
+            jobs: [...prev.saved.jobs, job],
+          },
+        };
+      }
+      return prev;
+    });
+  };
+  const unsaveJob = (jobRmv: Job) => {
+    setConfig((prevConfig) => ({
+      ...prevConfig,
+      saved: {
+        ...prevConfig.saved,
+        jobs: prevConfig.saved.jobs.filter((job) => job.id !== jobRmv.id), 
+      },
+    }));
+  };
+  const saveResume = (resume: Resume) => {
+    setConfig((prev) => {
+      const isSaved = prev.saved.resumes.some(
+        (savedResume) => savedResume.id === resume.id
+      );
+  
+      if (!isSaved) {
+        return {
+          ...prev,
+          saved: {
+            ...prev.saved,
+            resumes: [...prev.saved.resumes, resume],
+          },
+        };
+      }
+      return prev;
+    });
+  }
+  const unsaveResume = (resumeRmv:Resume) => {
+    setConfig((prevConfig) => ({
+      ...prevConfig,
+      saved: {
+        ...prevConfig.saved,
+        resumes: prevConfig.saved.resumes.filter((resume) => resume.id !== resumeRmv.id), 
+      },
+    }));
+  };
+  const newMessage = (msg:string) =>{
+    setConfig((prev)=>({...prev,message:msg}));
+  }
   return (
-    <ConfigContext.Provider value={{ config, setConfig }}>
+    <ConfigContext.Provider value={{ config, setConfig ,saveJob,unsaveJob,saveResume,unsaveResume,newMessage}}>
       {children}
     </ConfigContext.Provider>
   );
