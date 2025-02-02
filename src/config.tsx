58141055cbc85920f2496cde3daa1d6f24e63061
manxzo <manzoball@gmail.com>
2025-02-02 23:20:37 +0800

    cleaned up config

--- src/config.tsx
+++ src/config.tsx
@@ -1,4 +1,4 @@
-import { join } from "path";
+
 import React, { createContext, useState, ReactNode } from "react";
 
 interface Job {
@@ -31,7 +31,6 @@ interface Config {
     resumes:Resume[];
     jobs:Job[];
   }
-  message:string;
 }
 
 interface ConfigContextType {
@@ -41,7 +40,6 @@ interface ConfigContextType {
   unsaveJob:(job:Job) => void;
   saveResume:(resume:Resume) => void;
   unsaveResume:(resume:Resume)=>void;
-  newMessage:(message:string)=>void;
   isJobSaved:(job:Job)=>boolean;
   isResumeSaved:(resume:Resume)=>boolean;
 }
@@ -65,18 +63,16 @@ export const ConfigProvider = ({ children }: { children: ReactNode }) => {
       resumes:[],
       jobs:[],
     },
-    message:""
+
   });
-  const newMessage = (msg:string) =>{
-    setConfig((prev)=>({...prev,message:msg}));
-  }
+ 
   const saveJob = (job: Job) => {
     setConfig((prev) => {
       const isSaved = prev.saved.jobs.some(
         (favJob) => favJob.id === job.id
       );
       if (!isSaved) {
-        newMessage("Job Saved!")
+       
         return {
           ...prev,
           saved: {
@@ -85,7 +81,7 @@ export const ConfigProvider = ({ children }: { children: ReactNode }) => {
           },
         };
       }
-      newMessage("Job Already Saved")
+     
       return prev;
     });
   };
@@ -97,7 +93,7 @@ export const ConfigProvider = ({ children }: { children: ReactNode }) => {
         jobs: prevConfig.saved.jobs.filter((job) => job.id !== jobRmv.id), 
       },
     }));
-    newMessage("Job Deleted");
+
   };
   const saveResume = (resume: Resume) => {
     setConfig((prev) => {
@@ -106,7 +102,7 @@ export const ConfigProvider = ({ children }: { children: ReactNode }) => {
       );
   
       if (!isSaved) {
-        newMessage("Resume Saved!")
+ 
         return {
           ...prev,
           saved: {
@@ -115,7 +111,7 @@ export const ConfigProvider = ({ children }: { children: ReactNode }) => {
           },
         };
       }
-      newMessage("Resume Already Saved!")
+     
       return prev;
     });
   }
@@ -127,7 +123,7 @@ export const ConfigProvider = ({ children }: { children: ReactNode }) => {
         resumes: prevConfig.saved.resumes.filter((resume) => resume.id !== resumeRmv.id), 
       },
     }));
-    newMessage("Resume Deleted!")
+    
   };
 
   const isJobSaved = (job)=>{
@@ -143,7 +139,7 @@ export const ConfigProvider = ({ children }: { children: ReactNode }) => {
     return isSaved;
   }
   return (
-    <ConfigContext.Provider value={{ config, setConfig ,saveJob,unsaveJob,saveResume,unsaveResume,newMessage, isJobSaved,isResumeSaved}}>
+    <ConfigContext.Provider value={{ config, setConfig ,saveJob,unsaveJob,saveResume,unsaveResume,isJobSaved,isResumeSaved}}>
       {children}
     </ConfigContext.Provider>
   );
